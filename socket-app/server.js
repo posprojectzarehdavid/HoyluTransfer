28b447eef4f90d5d1b558b9bdc7f21b6b1e1d4e1
Zareh <z.zareh@gmail.com>
2017-07-25 14:39:49 +0200

    test14

--- socket-app/server.js
+++ socket-app/server.js
@@ -74,6 +74,7 @@ console.log('test10');
 console.log('test11');
 console.log('test12');
 console.log('test13');
+console.log('test14');
     socket.on('client', function (data) {
         console.log(data + ' connected...');
     });
