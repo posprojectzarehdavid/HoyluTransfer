d859713bf819218c893059ebec22ac511da91ab6
David <davidarminger@gmx.at>
2018-02-20 12:49:01 +0100

    HoyluLibrary überarbeitet

--- HoyluReceiver/Test/Program.cs
+++ HoyluReceiver/Test/Program.cs
@@ -11,9 +11,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Main seas = new Main();
-            seas.IpAddress = "40.114.246.211";
-            seas.Port = "4200";
+            Receiver seas = new Receiver("40.114.246.211", "4200");
+            seas.SavePath = @"C:\Users\David\Desktop\";
             seas.Register("TestUser");
             Console.ReadKey();
         }
